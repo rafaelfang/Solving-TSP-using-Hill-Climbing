784045bddf68d91ce5e825814441eed324aee065
rafaelfang <rafaelfang@live.com>
2014-10-06 17:46:49 -0500

    Hill climbing update 10/6/2014
    generate three more candidates and get the lowest cost candidate to be
    s_next state.
    get random length of city to be swaped

---
+++ getNextState.m
@@ -0,0 +1,26 @@
+function [ s_next ] = getNextState( s,totalCityNum )
+%random get the next state
+%written by Chao Fang
+    %get random city swap length, should exclude begining city
+    k=randi(totalCityNum-1);
+    %k=3;
+    index=1:totalCityNum+1;
+    start=1+randi(size(s,1)-2-k+1,1,1);
+    reversePart=flipud([start:start+k-1]');
+    index(start:start+k-1)=reversePart;
+    s_next=s(index,:);
+    disp([start,k])
+    
+    disp('============================')
+%     k=randi(totalCityNum-1);
+% first=1+randi(size(s,1)-2-k+1,1,1);
+% second=first+k-1;
+%     while first==second
+%         second=randi(size(s,1),1,1);
+%     end
+% temp=s(first,:);
+% s(first,:)=s(second,:);
+% s(second,:)=temp;
+% s_next=s;
+end
+
--- travellingSalesmanRunner.m
+++ travellingSalesmanRunner.m
@@ -1,20 +1,29 @@
+close all
+clear all
+clc
+
 %this is the runner function of travelling salesman problem using random hill
 %climbing
 %written by Chao Fang
-rng(1);
+%rng(1);
+%initalily select swap city string length to be seven
+
+
 %choice==1-fifteen cities
 %choice==2-fifty seven cities
-[coords,D]=prepareDataSet(2);
+[coords,D,totalCityNum]=prepareDataSet(2);
 cityID=1:size(coords,1);
 
 %start with any tour, e.g in input order
 %assign cityID to each coordinates
 s=[cityID', coords];
 
+%attach the begining city to the end of starting sequence.
+s=[s;s(1,:)];
 
 %disp('initial string of city ID with their coords:')
 %disp(s)
-
+max_evaluations=5000;
 figure;
 subplot(2,2,1);
 plot(s(:,2),s(:,3));
@@ -25,18 +34,37 @@ ylabel('y-axis')
 %record initial tour as best so far
 minCost=cost(s,D);
 minTour=s;
+candidateNum=3;
 
-max_evaluations=5000;
 num_evaluations=1;
 minCostArray=zeros(max_evaluations,1);
 while (num_evaluations<max_evaluations+1)
-    s_next=randomNextState(s);
-    if cost(s_next,D)<cost(s,D)
-        s=s_next;
+    
+   
+    candidate=zeros(size(s,1),size(s,2),candidateNum);
+    candidateCost=zeros(1,candidateNum);
+    for t=1:candidateNum
+        candidate(:,:,t)=getNextState(s,totalCityNum);
+        candidateCost(1,t)=cost(candidate(:,:,t),D);
     end
-    minCostArray(num_evaluations,1)=cost(s_next,D);
+    [minVal,minIndex]=min(candidateCost);
+    
+    if minVal<cost(s,D)
+        s=candidate(:,:,minIndex);
+    end
+    minCostArray(num_evaluations,1)=minVal;
     num_evaluations=num_evaluations+1;
+    %disp(s)
 end
+
+% while (num_evaluations<max_evaluations+1)
+%     s_next=getNextState(s,totalCityNum);
+%     if cost(s_next,D)<cost(s,D)
+%         s=s_next;
+%     end
+%     minCostArray(num_evaluations,1)=cost(s_next,D);
+%     num_evaluations=num_evaluations+1;
+% end
 %disp('after # of iteration:')
 %disp(num_evaluations-1);
 subplot(2,2,2)
